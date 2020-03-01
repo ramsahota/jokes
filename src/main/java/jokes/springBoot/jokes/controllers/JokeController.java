bc6bfe5d3132cc86a2a6c39c980da3b536ad1662
ronsahota <ronsahota@hotmail.com>
2020-03-01 14:10:36 -0500

    Built jokes app and added custom banner.

---
+++ src/main/java/jokes/springBoot/jokes/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package jokes.springBoot.jokes.controllers;
+
+import jokes.springBoot.jokes.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    //Spring will autowire here don't need to put in @Autowired annotation
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
---
+++ src/main/java/jokes/springBoot/jokes/services/JokeService.java
@@ -0,0 +1,5 @@
+package jokes.springBoot.jokes.services;
+
+public interface JokeService {
+    String getJoke();
+}
