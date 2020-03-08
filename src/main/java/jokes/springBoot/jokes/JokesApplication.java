93525acabfb3964f9c0aa5c2ab7c71e2eec22c36
ronsahota <ronsahota@hotmail.com>
2020-03-08 16:00:19 -0400

    Spring XML configuration.

--- src/main/java/jokes/springBoot/jokes/JokesApplication.java
+++ src/main/java/jokes/springBoot/jokes/JokesApplication.java
@@ -2,8 +2,10 @@ package jokes.springBoot.jokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokesApplication {
 
 	public static void main(String[] args) {
